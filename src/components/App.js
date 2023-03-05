7237b6ca5392fc88d99fe80b7ebc8c9452a7b553
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-03-05 10:23:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,20 @@ import React from 'react'
 import '../styles/App.css';
 
 class App extends React.Component {
-  
+    constructor(){
+        super();
+        this.name="John Doe"
+        this.age="34"
+        this.enrollmentNo="12345678"
+    }
+    render(){
+        return(
+            <div>
+                <h1>Hello, my name is {this.name} </h1>
+                <p>I am {this.age} years old and my enrollment no is {this.enrollmentNo}</p>
+            </div>
+        )
+    }
 }
 
 
